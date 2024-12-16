127920a07a9a17ae49af6697c3f3e4974735b792
Kyle Mabaso <kylemabaso@gmail.com>
2024-12-16 19:59:33 +0200

    Update show.blade.php

--- resources/views/pages/clients/show.blade.php
+++ resources/views/pages/clients/show.blade.php
@@ -218,7 +218,7 @@ Clients - {{ $client->name }}
                         <h6 class="note-title text-truncate w-75 mb-0" data-noteheading="{{ $note->title }}">
                             {{ $note->title }}
                         </h6>
-                        <p class="note-date fs-2">{{ $note->created_at }}/p>
+                        <p class="note-date fs-2">{{ $note->created_at }}</p>
                         <div class="note-content">
                             <p class="note-inner-content" data-notecontent="{{ $note->content }}">
                                 {{ $note->content }}
@@ -231,55 +231,55 @@ Clients - {{ $client->name }}
                             <a href="javascript:void(0)" class="link text-danger ms-2">
                                 <i class="ti ti-trash fs-4 remove-note"></i>
                             </a>
-                            <div class="ms-auto">
-                                <div class="category-selector btn-group">
-                                    <a class="nav-link category-dropdown label-group p-0" data-bs-toggle="dropdown"
-                                        href="javascript:void(0)" role="button" aria-haspopup="true" aria-expanded="false">
-                                        <div class="category">
-                                            <div class="category-business"></div>
-                                            <div class="category-social"></div>
-                                            <div class="category-important"></div>
-                                            <span class="more-options text-dark">
-                                                <i class="ti ti-dots-vertical fs-5"></i>
-                                            </span>
+                            <!-- <div class="ms-auto">
+                                    <div class="category-selector btn-group">
+                                        <a class="nav-link category-dropdown label-group p-0" data-bs-toggle="dropdown"
+                                            href="javascript:void(0)" role="button" aria-haspopup="true" aria-expanded="false">
+                                            <div class="category">
+                                                <div class="category-business"></div>
+                                                <div class="category-social"></div>
+                                                <div class="category-important"></div>
+                                                <span class="more-options text-dark">
+                                                    <i class="ti ti-dots-vertical fs-5"></i>
+                                                </span>
+                                            </div>
+                                        </a>
+                                        <div class="dropdown-menu dropdown-menu-right category-menu" style="">
+                                            <a class="
+                                                                                                                                          note-business
+                                                                                                                                          badge-group-item badge-business
+                                                                                                                                          dropdown-item
+                                                                                                                                          position-relative
+                                                                                                                                          category-business
+                                                                                                                                          d-flex
+                                                                                                                                          align-items-center
+                                                                                                                                        "
+                                                href="javascript:void(0);">Business</a>
+                                            <a class="
+                                                                                                                                          note-social
+                                                                                                                                          badge-group-item badge-social
+                                                                                                                                          dropdown-item
+                                                                                                                                          position-relative
+                                                                                                                                          category-social
+                                                                                                                                          d-flex
+                                                                                                                                          align-items-center
+                                                                                                                                        "
+                                                href="javascript:void(0);">
+                                                Social</a>
+                                            <a class="
+                                                                                                                                          note-important
+                                                                                                                                          badge-group-item badge-important
+                                                                                                                                          dropdown-item
+                                                                                                                                          position-relative
+                                                                                                                                          category-important
+                                                                                                                                          d-flex
+                                                                                                                                          align-items-center
+                                                                                                                                        "
+                                                href="javascript:void(0);">
+                                                Important</a>
                                         </div>
-                                    </a>
-                                    <div class="dropdown-menu dropdown-menu-right category-menu" style="">
-                                        <a class="
-                                                                                                                                      note-business
-                                                                                                                                      badge-group-item badge-business
-                                                                                                                                      dropdown-item
-                                                                                                                                      position-relative
-                                                                                                                                      category-business
-                                                                                                                                      d-flex
-                                                                                                                                      align-items-center
-                                                                                                                                    "
-                                            href="javascript:void(0);">Business</a>
-                                        <a class="
-                                                                                                                                      note-social
-                                                                                                                                      badge-group-item badge-social
-                                                                                                                                      dropdown-item
-                                                                                                                                      position-relative
-                                                                                                                                      category-social
-                                                                                                                                      d-flex
-                                                                                                                                      align-items-center
-                                                                                                                                    "
-                                            href="javascript:void(0);">
-                                            Social</a>
-                                        <a class="
-                                                                                                                                      note-important
-                                                                                                                                      badge-group-item badge-important
-                                                                                                                                      dropdown-item
-                                                                                                                                      position-relative
-                                                                                                                                      category-important
-                                                                                                                                      d-flex
-                                                                                                                                      align-items-center
-                                                                                                                                    "
-                                            href="javascript:void(0);">
-                                            Important</a>
                                     </div>
-                                </div>
-                            </div>
+                                </div> -->
                         </div>
                     </div>
                 </div>
